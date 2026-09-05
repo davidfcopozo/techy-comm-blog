8bcd335bd3890778e9b2202da3e5dfd95b5fdfe8
David <davidfco.pozo@gmail.com>
2026-09-05 12:19:19 +0200

    Restrict custom DNS resolver to development environment
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- api/src/config/connect.ts
+++ api/src/config/connect.ts
@@ -2,7 +2,13 @@ import mongoose, { ConnectOptions } from "mongoose";
 import dns from "dns";
 
 export const connectDB = (url: string) => {
-  dns.setServers(["8.8.8.8", "1.1.1.1"]);
+  if (process.env.NODE_ENV === "development") {
+    try {
+      dns.setServers(["8.8.8.8", "1.1.1.1"]);
+    } catch (e) {
+      // Ignore DNS override errors
+    }
+  }
 
   return mongoose.connect(url, {
     useNewUrlParser: true,
