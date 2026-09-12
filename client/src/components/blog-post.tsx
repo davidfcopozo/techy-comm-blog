746bd61a9c7423c249b5244cb05c3a6506b777a9
David <davidfco.pozo@gmail.com>
2026-09-12 15:39:36 +0200

    Integrate RelatedPosts component into blog post view layout
    Signed-off-by: David <davidfco.pozo@gmail.com>

--- client/src/components/blog-post.tsx
+++ client/src/components/blog-post.tsx
@@ -8,6 +8,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React, { memo, useEffect, useState } from "react";
 import CommentSection from "./comment-section";
+import RelatedPosts from "./related-posts";
 import { UserType } from "@/typings/types";
 import ContentRenderer from "./ui/content-renderer";
 import { EngagementButton } from "./engagement-button";
@@ -328,6 +329,7 @@ const BlogPost = memo(function BlogPost({
                   className="w-full sm:w-[95%] md:w-[90%] lg:w-[85%] pt-4 mx-auto max-w-full"
                 />
               </div>
+              <RelatedPosts currentPost={post} limit={3} />
               <CommentSection
                 comments={post.comments || []}
                 post={post}
